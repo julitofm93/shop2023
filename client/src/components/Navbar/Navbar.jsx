629384766b46b884a483bc90cd012d2a547d9654
julitofm93 <julian.fm@live.com>
2023-03-16 14:11:48 -0300

    navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+{/*             <img src="/img/en.png" /> */}
+          <ArrowDropDown/>
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">JULITOSTORE</Link>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
   )
 }
 
